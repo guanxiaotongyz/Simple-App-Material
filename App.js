c2d8925ccb0357b260d9c9e14465c1e453411820
Xiaotong Guan <xiaotongguan@XiaotongdeMacBook-Pro.local>
2023-03-07 16:24:33 -0800

    add background lineargradient

--- App.js
+++ App.js
@@ -9,13 +9,13 @@ import ShapeBanCai from "./calculatecomponents/ShapeBanCai";
 import ShapeYuanBang from "./calculatecomponents/ShapeYuanBang";
 import ShapeYuanGuan from "./calculatecomponents/ShapeYuanGuan";
 import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   const [type, setType] = React.useState("NAN");
   const [density, setDensity] = React.useState("");
   const [shape, setShape] = React.useState("");
   const [unitPrice, setUnitPrice] = React.useState("");
-
 
   function renderType(text) {
     setType(text);
@@ -36,12 +36,10 @@ export default function App() {
     setUnitPrice(text);
   }
 
-
   console.log("===Type", type);
   // console.log("===Density", density);
   // console.log("===Shape", shape);
   // console.log("===UnitPrice", unitPrice);
-  
 
   let screen = null;
 
@@ -50,21 +48,33 @@ export default function App() {
     screen = <ShapeBanCai infomaterial={{ type, density, shape, unitPrice }} />;
   } else if (shape === "圆棒") {
     console.log("===圆棒===");
-    screen = <ShapeYuanBang infomaterial={{ type, density, shape, unitPrice }} />;
+    screen = (
+      <ShapeYuanBang infomaterial={{ type, density, shape, unitPrice }} />
+    );
   } else if (shape === "圆管") {
-    screen = <ShapeYuanGuan infomaterial={{ type, density, shape, unitPrice }} />;
+    screen = (
+      <ShapeYuanGuan infomaterial={{ type, density, shape, unitPrice }} />
+    );
     console.log("===圆管===");
   }
 
   return (
-    <View style={styles.container}>
-      <TitleText />
-      <MaterialType infosave={renderType} />
-      <MaterialDensity infosavedensity={renderDensity} typename={{type}}/>
-      <MaterialUnitPrice infosaveUnitPrice={renderUnitPrice}/>
-      <MaterialShape infosaveshape={renderShape} />
-      {screen}
-    </View>
+    <LinearGradient
+      // Background Linear Gradient
+      colors={["white", "lightblue"]}
+      style={styles.container}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+    >
+      <View style={styles.container}>
+        <TitleText />
+        <MaterialType infosave={renderType} />
+        <MaterialDensity infosavedensity={renderDensity} typename={{ type }} />
+        <MaterialUnitPrice infosaveUnitPrice={renderUnitPrice} />
+        <MaterialShape infosaveshape={renderShape} />
+        {screen}
+      </View>
+    </LinearGradient>
   );
 }
 
